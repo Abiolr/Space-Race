680ed6aff2911ddbcfccd999e220755f6781ff41
Kiba727 <dangpatrick14@gmail.com>
2024-10-19 15:30:36 -0600

    the work we did when we met up on thursday

--- Spacerace/MODEL.C
+++ Spacerace/MODEL.C
@@ -16,31 +16,31 @@ void init_asteroid(Asteroid *asteroid, unsigned int x, unsigned int y, int delta
 
 void init_model(Model *model)
 {
-    init_spaceship(&model->spaceship);
-
     int i;
     int j = 0;
     int k = 1;
 
-    int x[] = {344, 8, 504, 216, 88, 600, 280, 168, 392, 24,
+    int asteroid_x_positions[] = {344, 8, 504, 216, 88, 600, 280, 168, 392, 24,
                 440, 104, 376, 264, 152, 632, 200, 40, 536, 280,
                 280, 392, 152, 616, 72, 328, 488, 440, 136, 392,
-                440, 280, 24, 488, 88};
+                440, 280, 24};
+
+    init_spaceship(&model->spaceship);
+
+    model->lives = 3;
+    model->score = 0;
 
     for (i = 0; i < 33; i += 3)
     {
         int y = (j << 5) + 8;
 
-        init_asteroid(&model->asteroid, x[i], y, 2 * k);
-        init_asteroid(&model->asteroid, x[i+1], y, 2 * k);
-        init_asteroid(&model->asteroid, x[i+2], y, 2 * k);
+        init_asteroid(&model->asteroid[i], asteroid_x_positions[i], y, 2 * k);
+        init_asteroid(&model->asteroid[i+1], asteroid_x_positions[i+1], y, 2 * k);
+        init_asteroid(&model->asteroid[i+2], asteroid_x_positions[i+2], y, 2 * k);
 
         j += 1;
         k *= -1;
         }
-    
-    int lives = 3;
-    int score = 0;
 }
 
 void move_asteroid(Asteroid *asteroid)
@@ -51,5 +51,11 @@ void move_asteroid(Asteroid *asteroid)
 void move_spaceship(Spaceship *spaceship)
 {
     spaceship->y += spaceship->delta_y;     /*the y is moves delta y spaces*/
+}
+
+void reset_spaceship(Spaceship *spaceship)
+{
+    spaceship->x = 320;
+    spaceship->y = 350;
 }
 
--- Spacerace/TST_RAST.C
+++ Spacerace/TST_RAST.C
@@ -1,11 +1,32 @@
 #include <osbind.h>
 #include "raster.h"
-
-
+#include "events.h"
+#include "model.h"
 
 int main()
 {
+    Model model;
+    init_model(&model);
 
+    printf("Initial Spaceship position: x=%d, y=%d\n", model.spaceship.x, model.spaceship.y);
+
+    move_spaceship_up(&model);
+    move_spaceship(&model.spaceship);
+    printf("Spaceship after moving up: x=%d, y=%d\n", model.spaceship.x, model.spaceship.y);
+
+    move_spaceship_down(&model);
+    move_spaceship(&model.spaceship);
+    printf("Spaceship after moving down: x=%d, y=%d\n", model.spaceship.x, model.spaceship.y);
+
+    update_asteroids(&model);
+    printf("Asteroid[0] after move: x=%d, y=%d\n", model.asteroid[0].x, model.asteroid[0].y);
+
+    check_collisions(&model);
+    printf("Lives remaining: %d\n", model.lives);
+
+    return 0;
+
+/*
     int i;
     int j = 0;
     int k = 1;
@@ -13,7 +34,7 @@ int main()
     int x[] = {344, 8, 504, 216, 88, 600, 280, 168, 392, 24,
                 440, 104, 376, 264, 152, 632, 200, 40, 536, 280,
                 280, 392, 152, 616, 72, 328, 488, 440, 136, 392,
-                440, 280, 24, 488, 88};
+                440, 280, 24};
                 
     clear_screen();
 
@@ -38,4 +59,5 @@ int main()
     plot_heart(600,50);
     plot_heart(580,50);
     return 0;
+    */
 }
