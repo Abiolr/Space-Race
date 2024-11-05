dd08cf4c922b13aeadbfbe99cd9d57521d8eea9c
Patrick Dang <113149855+Kiba727@users.noreply.github.com>
2024-11-05 01:01:39 -0700

    Update TST_REND.C

--- Spacerace/TST_REND.C
+++ Spacerace/TST_REND.C
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <osbind.h>
 #include "RENDER.H"
 #include "MODEL.H"
+#include "EVENTS.C"
+#include "raster.h"
+
 
 int main(){
 
+    char input;
+
     Model model;
     init_model(&model);
-    render(&model);
+    clear_screen();
 
+    input = Cnecin();
+
+    while (input != ESC)
+    {
+        switch(input) {
+            case w:
+            move_spaceship_up(&model);
+            move_spaceship(&model.spaceship);
+            render(&model);
+            break;
+            case s:
+            move_spaceship_down(&model);
+            move_spaceship(&model.spaceship);
+            render(&model);
+            break;
+            case SPACE:
+            update_asteroids(&model);
+            render(&model);
+            break;
+            }
+        input = Cnecin();
+    }
     return 0;
 }
-
